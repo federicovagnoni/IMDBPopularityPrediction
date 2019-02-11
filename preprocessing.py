b88e1cfc91766de00cca7164decc811f7e730871
emanuele <emanuele.vannacci@gmail.com>
2019-02-11 11:28:27 +0100

    Working on preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -2,17 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 
+# #############################################################################
+# Load data
 credits = pd.read_csv("dataset/tmdb_5000_credits.csv")
 movies = pd.read_csv("dataset/tmdb_5000_movies.csv")
 
+# #############################################################################
+# Popularity Histogram
 popularity = movies.popularity
 print(popularity.describe())
 
 plt.hist(popularity, bins=100)
-# plt.show()
+plt.show()
 
+# #############################################################################
+# Create dictionary: key: production company; value: mean popularity
 companiesList = dict()
-
 for index, row in movies.iterrows():
     companies = row['production_companies']
     companiesJson = json.loads(companies)
@@ -23,24 +28,41 @@ for index, row in movies.iterrows():
         else:
             companiesList[c['name']] = [row['popularity'], 1]
 
+# Compute mean
 for e in companiesList.keys():
-    companiesList[e] = [companiesList[e][0] / companiesList[e][1]]
+    companiesList[e] = companiesList[e][0] / companiesList[e][1]
 
-df = pd.DataFrame.from_dict(companiesList, orient='index')
-df = df.sort_values(0, ascending=False)
-df = df.head(20).sort_values(0, ascending=True)
+# df = pd.DataFrame.from_dict(companiesList, orient='index')
+# df = df.sort_values(0, ascending=False)
+# print(df.head())
+# df = df.head(20).sort_values(0, ascending=True)
+# companies = df.index.values
+# print(companies)
 
-companies = df.index.values
-print(companies)
+# #############################################################################
+# Load data
+# for company in companies:
+#     movies[company] = 0
+#     for index, row in movies.iterrows():
+#         companies = row['production_companies']
+#         companiesJson = json.loads(companies)
+#         names = [c['name'] for c in companiesJson]
+#         if company in names:
+#             movies.at[index, company] = 1
+#
+# movies.to_csv('./dataset/movies_companies20.csv', index=False)
 
-for company in companies:
-    movies[company] = 0
-    for index, row in movies.iterrows():
-        companies = row['production_companies']
-        companiesJson = json.loads(companies)
-        names = [c['name'] for c in companiesJson]
-        if company in names:
-            movies.at[index, company] = 1
+movies['companies_popularity'] = 0
+for index, row in movies.iterrows():
+    companies = row['production_companies']
+    companiesJson = json.loads(companies)
+    names = [c['name'] for c in companiesJson]
+    for company in names:
+            movies.at[index, 'companies_popularity'] += companiesList[company]
 
-movies.to_csv('./dataset/movies_companies20.csv', index=False)
+    if movies.at[index, 'companies_popularity'] != 0:
+        movies.at[index, 'companies_popularity'] /= len(names)
+
+print(movies['companies_popularity'].describe())
+movies.to_csv('./dataset/movies_companies.csv', index=False)
 exit()
